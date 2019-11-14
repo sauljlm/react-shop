3bc1f548c3befcb5589f9623ba485898480fa657
Saul Lopez <saul.json@gmail.com>
2019-11-14 17:40:03 -0600

    reescribo el readme, cambio imagenes

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <header className="header">
-      <Link className="header__logo" to="/" title="link to home" ><img src="./static/logo2.jpg" alt=""/></Link>
+      <Link className="header__logo" to="/" title="link to home" ><img src="./static/logo2.png" alt=""/></Link>
       <Nav />
       <Link className="header__shoping-car" to="/shopping-car">
         <img src="./static/shopping-car.png" alt=""/>
